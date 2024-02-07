9bebdf21c1a07f0f863b563fc3f85f5e4710cd28
mrcallaghan <mcallaghan@naperville203.org>
2024-02-07 16:11:05 -0600

    Began writing FillInQuestion

---
+++ FillInQuestion.java
@@ -0,0 +1,37 @@
+
+/**
+ * A FillInQuestion is constructed with a string that contains the answer surrounded by
+ *   "_". For example, "The inventor of Java is _James Gosling_."  The question should be 
+ *   displayed as: "The inventor of Java is _______________."
+ *
+ * @author mrcallaghan
+ * @version 07feb2024
+ */
+
+/*
+ * The FillInQuestion class extends (i.e., is a subclass of, is derived from) the Question class.
+ */
+public class FillInQuestion extends Question
+{
+    /*
+     * DO NOT declare the instance variables for the text and answer.
+     * 
+     * The text and answer instance variables are inherited from the Quesiton class.
+     */
+    
+    
+    /*
+     * 
+     */
+    @Override
+    public void setText(String questionText)
+    {
+        
+    }
+}
+
+
+
+
+
+
