1f188e5dff2d41b2ec16cc9392370a33b17620af
mrcallaghan <mcallaghan@naperville203.org>
2024-02-08 14:45:10 -0600

    Wrote the setText method for a FillInQuesiton

--- FillInQuestion.java
+++ FillInQuestion.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * A FillInQuestion is constructed with a string that contains the answer surrounded by
  *   "_". For example, "The inventor of Java is _James Gosling_."  The question should be 
@@ -21,11 +21,45 @@ public class FillInQuestion extends Question
     
     
     /*
+     * This method overrides the setText method in the Question class.
      * 
+     * Sets the question text and answer for a FillInQuestion.
+     * 
+     * @param questionText the text of the question, including the answer
      */
     @Override
     public void setText(String questionText)
     {
+        Scanner parser = new Scanner(questionText);
+        parser.useDelimiter("_");
+        String question = parser.next();  //  "The inventor of Java is "
+        String answer = parser.next();  // "James Gosling"
+        question += "_________________" + parser.next();  // "____________."
+        
+        /*
+         * The inherited instance variables are private; they cannot be accessed directly.
+         *      We must use the mutator and accessor methods.
+         */
+        //this.text = question;
+        //this.answer = answer;
+        
+        /*
+         * Use the "super" reserved word to call the setText method as defined in the 
+         *      superclass (e.g., Question).
+         */
+        super.setText(question);
+        
+        /*
+         * We should use 'this' reserved word to call the setAnswer method.  If the 
+         *      subclass doesn't override the setAnswer method, the superclass's method
+         *      will be called.
+         *      
+         *    We don't use the 'super' reserved word in this case, because if we later override the
+         *      setAnswer method, the overridden method would not be called.
+         */
+        this.setAnswer(answer);
+        
+        
         
     }
 }
