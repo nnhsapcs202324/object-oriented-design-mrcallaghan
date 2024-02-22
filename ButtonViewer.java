9d000c0afed456f3ee72acc85aec5768a79b1cb7
mrcallaghan <mcallaghan@naperville203.org>
2024-02-22 15:01:01 -0600

    Refactored code to use an inner class

--- ButtonViewer.java
+++ ButtonViewer.java
@@ -2,6 +2,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 /**
  * Demonstrates installing an action listener in a GUI app
  *
@@ -12,34 +14,55 @@ public class ButtonViewer
 {
     private final int FRAME_WIDTH = 400;
     private final int FRAME_HEIGHT = 100;
-    
+
     private JFrame frame;
     private JPanel panel;
     private JButton button;
-    
+    private JLabel label;
+    private int clickCount;
+
     public ButtonViewer()
     {
+        this.clickCount = 0;
+        
         // 1. define and setup the UI components
         this.frame = new JFrame();
         this.panel = new JPanel();
-        
+
         this.button = new JButton("click me");
-        
+
         this.panel.add(this.button);
-        this.frame.add(this.panel);
         
+        this.label = new JLabel("0 clicks");
+        this.panel.add(label);
+        
+        
+        this.frame.add(this.panel);
+
         // 2. create listener objects
         ClickListener listener = new ClickListener();
-        
+
         // 3. register the listeners with the components that generate events
         this.button.addActionListener(listener);
-        
+
         this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setVisible(true);
-        
+
     }
-    
+
+    public class ClickListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent event)
+        {
+            // 4. Respond to events that are generated
+            //System.out.println("Button clicked");
+            clickCount++;
+            label.setText(clickCount + " clicks");
+        }
+    }
+
     public static void main(String[] args)
     {
         ButtonViewer viewer = new ButtonViewer();
