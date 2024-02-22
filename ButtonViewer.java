caa64ff10d2388735b6856bf5baa53912bca64b3
mrcallaghan <mcallaghan@naperville203.org>
2024-02-22 08:24:18 -0600

    updated button to capture click count

--- ButtonViewer.java
+++ ButtonViewer.java
@@ -2,6 +2,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * This class demonstrates how to install an an action listener in a GUI app.
@@ -13,20 +15,28 @@ public class ButtonViewer
 {
     private final int FRAME_WIDTH = 400;
     private final int FRAME_HEIGHT = 100;
-    
+
     private JFrame frame;
     private JPanel panel;
     private JButton button;
+    private JLabel label;
     
+    private int clickCount;
+
     public ButtonViewer()
     {
+        this.clickCount = 0;
+        
         // 1. define and setup the UI components
         this.frame = new JFrame();
         this.panel = new JPanel();
-        
+
         this.button = new JButton("Click Me!");
         this.panel.add(this.button);
         
+        this.label = new JLabel("0 clicks");
+        this.panel.add(this.label);
+
         this.frame.add(this.panel);
 
         // 2. create listener objects
@@ -34,14 +44,23 @@ public class ButtonViewer
 
         // 3. register listener object with the component that generates events
         this.button.addActionListener(listener);
-        
-        
+
         this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setVisible(true);
 
     }
-    
+
+    public class ClickListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent event)
+        {
+            clickCount++;
+            label.setText(clickCount + " clicks");
+        }
+    }
+
     public static void main(String[] args)
     {
         ButtonViewer viewer = new ButtonViewer();
